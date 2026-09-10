ffc6965c5a0e529b73013e9d6fe2b1aeea0ac893
Bhairulal Teli <bhairulalteli07@gmail.com>
2026-09-10 09:04:37 +0530

    Describe your new changes

--- src/components/edit-log/edit-log.ts
+++ src/components/edit-log/edit-log.ts
@@ -1,5 +1,13 @@
 import { Component, computed, input, output, signal } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-log',
@@ -10,7 +18,7 @@ import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angu
 export class EditLog {
   cancel = output<void>();
   route = input.required<string>();
-  isFormValid= signal(false);
+  isFormValid= signal(true);
 
   log = new FormControl('', Validators.required);
 
@@ -27,9 +35,19 @@ export class EditLog {
     this.cancel.emit();
   }
 
-  isValid = computed(() => {
-    this.isFormValid.set(this.log.status === 'VALID');
-    return !this.isFormValid;
-  })
+  routeValidator(): ValidatorFn {
+    return (
+      control: AbstractControl
+    ): ValidationErrors | null => {
+      const value = control.value;
+
+      if (!value) {
+        return null;
+      }
+
+      const isValid = /^\/[a-zA-Z0-9-]+(\/[a-zA-Z0-9-]+)*$/.test(value);
+      return isValid ? null : { invalidRoute: true };
+    };
+  }
 
 }
